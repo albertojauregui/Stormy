24c22d69ec1cdd59665c1249e0d49bff53a407ea
Alberto Jauregui <bastertwo@gmail.com>
2014-12-21 10:15:25 -0700

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Alberto Jauregui on 21/12/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
